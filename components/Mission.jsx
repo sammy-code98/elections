9f2f5f56f607625984af3ff87a0b913d3f9e3347
sammy-code98 <sammysmart98@yahoo.com>
2022-02-13 17:41:54 +0100

    fix:reformat mission text

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -3,52 +3,60 @@ export default function Mission() {
   return (
     <section className="relative mt-44 md:mx-auto" id="mission">
       <div className="text-center text-sm text-gray-500 ">
-        GOVERNORSHIP CAMPAIGN SUPPORT COMMITTEE – MISSION STATEMENT
+        WHY ENGR. CHIEF ELIAS MBAM SHOULD BE THE CHOICE OF EBONYI PEOPLE COME
+        2023.{" "}
       </div>
-      <div className="text-center text-gray-800 text-2xl mt-2">MBAM ELIAS NETWORK</div>
+      <div className="text-center text-gray-800 text-xl mt-2">
+        MBAM ELIAS NETWORK GOVERNORSHIP CAMPAIGN SUPPORT COMMITTEE – MISSION
+        STATEMENT
+      </div>
 
       <div className="grid  grid-cols-1rid md:grid-cols-2 md:gap-8 justify-items-center md:px-14 mt-12">
-
         <div className="w-full bg-green-600 text-white rounded-md shadow-lg  mb-8 p-8">
           <ul className="list-disc list-inside">
-            <li>
-              Coordinating communication between Engr. Elias Mbam Nwalem and the masses.
-            </li>
-            <li>Provide support to state entities for the campaign process</li>
-            <li>
-              Mobilise Nigerians from different states to support the Engr. Elias Mbam Nwalem
-              Governorship Team
-            </li>
-            <li>
-              Prepare and administer an exploratory/outreach plan to Nigerians
-              that provides needed information to Engr. Elias Mbam Nwalem and solicit
-              meaningful information from them.
-            </li>
-            <li>
-              As a matter of course, take grassroots input into account, and
-              have as overarching goal the growth of the  Engr. Elias Mbam Nwalem
-              Governorship Team
-            </li>
+            <div>
+              <li>A MAN OF DUE PROCESS:</li>
+              <p>
+                Due process simply means strict compliance/adherence to and
+                following established lay down rules/laws in getting things done
+                which running of a government is among. Tell me a man who
+                observes and respects rules and laws in his doings, and I&apos; ll
+                show you a man who will respect your rights and accord you all
+                your privileges. It&apos; s important to note that Engr. Chief Elias
+                Mbam is a product of quality trainings in both civil and public
+                service, and understands and appreciates the importance and the
+                fact that governance becomes easier when leadership is in strict
+                compliance to the rule of law, hence every public and civil
+                servant in the state is sure to enjoy all his rights and
+                privileges under his administration. As a product and a
+                beneficiary of well structured strong institutions, Chief Mbam
+                will restructure and reposition the civil service system for a
+                better and effective service delivery to Ebonyi people. With
+                Mbam, our rights shall be respected and privileges assuredly
+                guaranteed.
+              </p>
+            </div>
           </ul>
         </div>
         <div className="w-full bg-green-600 text-white rounded-md shadow-lg mb-8 p-8">
           <ul className="list-disc list-inside">
             <li>
-              Coordinating communication between Engr. Elias Mbam Nwalem and the masses.
+              Coordinating communication between Engr. Elias Mbam Nwalem and the
+              masses.
             </li>
             <li>Provide support to state entities for the campaign process</li>
             <li>
-              Mobilise Nigerians from different states to support the  Engr. Elias Mbam Nwalem
-              Governorship Team
+              Mobilise Nigerians from different states to support the Engr.
+              Elias Mbam Nwalem Governorship Team
             </li>
             <li>
               Prepare and administer an exploratory/outreach plan to Nigerians
-              that provides needed information to Engr. Elias Mbam Nwalem and solicit
-              meaningful information from them.
+              that provides needed information to Engr. Elias Mbam Nwalem and
+              solicit meaningful information from them.
             </li>
             <li>
               As a matter of course, take grassroots input into account, and
-              have as overarching goal the growth of the  Engr. Elias Mbam Nwalem
+              have as overarching goal the growth of the Engr. Elias Mbam Nwalem
               Governorship Team
             </li>
           </ul>
