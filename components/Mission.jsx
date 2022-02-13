66b3dae073ea098018eb585c6f940dc4ed0b2e31
sammy-code98 <sammysmart98@yahoo.com>
2022-02-13 20:44:22 +0100

    fix:update accordion content

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -3,19 +3,19 @@ export default function Mission() {
   return (
     <section className="relative mt-44 md:mx-auto" id="mission">
       <div className="text-center text-sm text-gray-500 ">
-        WHY ENGR. CHIEF ELIAS MBAM SHOULD BE THE CHOICE OF EBONYI PEOPLE COME
+        Why Engr. Chief Elias Mbam Should be the Choice of Ebonyi People Come
         2023.{" "}
       </div>
       <div className="text-center text-gray-800 text-xl mt-2">
-        MBAM ELIAS NETWORK GOVERNORSHIP CAMPAIGN SUPPORT COMMITTEE – MISSION
-        STATEMENT
+        Mbam Elias Network Governorship Campaign Support Committee ~ Mission
+        Statement
       </div>
 
       <div className="flex flex-col  mt-4 md:px-32 pt-4">
         <div className="collapse w-full bg-green-500 rounded-box shadow-md mb-4 collapse-arrow">
           <input type="checkbox" />
-          <div className="collapse-title text-base text-white font-medium">
-            A MAN OF DUE PROCESS{" "}
+          <div className="collapse-title text-lg text-white font-medium">
+            A man of due process{" "}
           </div>
           <div className="collapse-content text-white">
             <p>
@@ -39,12 +39,12 @@ export default function Mission() {
           </div>
         </div>
 
-        <div className="collapse w-full bg-green-500 rounded-box shadow-md mb-4 collapse-arrow">
+        <div className="collapse w-full bg-white rounded-box shadow-md mb-4 collapse-arrow">
           <input type="checkbox" />
-          <div className="collapse-title text-base text-white font-medium">
-            VERY WELL EXPERIENCED{" "}
+          <div className="collapse-title text-lg text-green-500 font-medium">
+            Very well experienced{" "}
           </div>
-          <div className="collapse-content text-white">
+          <div className="collapse-content text-green-500">
             <p>
               With every sense of modesty, Engr. Elias Mbam is one of the most
               experienced Ebonyian today most riped for governance and
@@ -54,12 +54,123 @@ export default function Mission() {
               responsibilities. In all of the positions and his engagements in
               civil and public services, he has always distinguished himself in
               discharging every of his assigned duties. As a seasoned civil
-              servant, he&apos;s a trained beurocrat and as a long serving public
-              servant on the corridors of power, he&apos;s an excellent manager of
-              both human and material resources. Anyone that combines the above
-              two qualities will surely score A1 in practical leadership, and
-              Ebonyians are blessed to have one in the person of Engr Chief
-              Elias Mbam.
+              servant, he&apos;s a trained beurocrat and as a long serving
+              public servant on the corridors of power, he&apos;s an excellent
+              manager of both human and material resources. Anyone that combines
+              the above two qualities will surely score A1 in practical
+              leadership, and Ebonyians are blessed to have one in the person of
+              Engr Chief Elias Mbam.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse w-full bg-green-500 rounded-box shadow-md mb-4 collapse-arrow">
+          <input type="checkbox" />
+          <div className="collapse-title text-lg text-white font-medium">
+            Very Principled{" "}
+          </div>
+          <div className="collapse-content text-white">
+            <p>
+              Every principled man is always a man with good conscience, and a
+              man of good conscience is always a man that has the fear of God.
+              Any leader with the fear of God doesn&apos;t lead with a
+              vindictive mindset, and when a leader doesn&apos;t lead with a
+              vindictive mindset, all citizens irrespective of tribe, clan,
+              kindred or political affiliation remain equal before the law and
+              the government, and are treated with every respect and honor one
+              deserves based on merit and not on where ones comes from or the
+              political party he belongs to. Chief Elias Mbam is a well
+              principled devoted Christian with the fear of God and good
+              conscience. Under his administration, Ebonyi people will be
+              treated equally, and social amenities distributed evenly. This can
+              only be seen in an administration of a man of integrity with good
+              conscience leading with the fear of God like Engr. Chief Mbam.
+            </p>
+          </div>
+        </div>
+        <div className="collapse w-full bg-white rounded-box shadow-md mb-4 collapse-arrow">
+          <input type="checkbox" />
+          <div className="collapse-title text-lg text-green-500 font-medium">
+            He&apos;s selfless to the core
+          </div>
+          <div className="collapse-content text-green-500">
+            <p>
+              Every selfless person is always a non greedy person, and the
+              greatest gift God will give to a people is a gift of a selfless
+              person as a leader. A selfless leader doesn&apos;t personalize
+              public property and fund, and wherever such a leader leads, the
+              people rejoice and the land witnesses unprecedented development
+              and progress. From his track records and pedigree, Chief Mbam is
+              an outstanding selfless Nigerian. He may not be throwing money
+              around, but he&apos;s definitely not in the class of those who
+              steal billions from you and come back to throw few notes of naira
+              at you. As a civil servant, Chairman of Boards, Commissioner,
+              Minister and now the Chairman of RMAFC, he has always put the
+              interest of the state ahead of his in all his undertakings. We
+              need a man who will give to God what belongs to Him and to Caesar
+              what belongs to him. In his administration, no Peter will be
+              robbed to pay Paul, everyone will get his due as provided by the
+              rules and laws of the state.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse w-full bg-green-500 rounded-box shadow-md mb-4 collapse-arrow">
+          <input type="checkbox" />
+          <div className="collapse-title text-lg text-white font-medium">
+            Will Diversify Natural Resources{" "}
+          </div>
+          <div className="collapse-content text-white">
+            <p>
+              It&apos;s not known fact that Ebonyi state is so blessed with
+              natural deposits and mineral resources lying dormant. Little or no
+              little do Ebonyians knew that diversification of this natural will
+              improve the living standard of the citizens. This mineral deposit
+              located in every local government Area of the state and rural
+              areas of the state, if properly harnessed will facelift the rural
+              Area and reduce rural -urban migration. Considering the enormous
+              endowment in this minerals will also contribute to the development
+              of the state and create more employments opportunities for
+              citizens of the state. The diversification of this mineral
+              resources will veritably increase the internal generated Revenue
+              of the state which will reflect on gross domestic product GDP
+              growth in the state. It&apos;s believed that for a state to
+              witness such development and progress it needs a leader with wide
+              experience and pedigree in fiscal works management and that man is
+              Engr Elias Mbam. Engr Elias Mbam having served as the commissioner
+              for works and minister for budget and planning, currently the
+              chairman RMAFC will savage every necessary tools available and
+              channel them towards harnessing the natural deposits to improve
+              the living standard of every Ebonyi citizens.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse w-full bg-white rounded-box shadow-md mb-4 collapse-arrow">
+          <input type="checkbox" />
+          <div className="collapse-title text-lg text-green-500 font-medium">
+            He&apos;s a Symbol of Unity
+          </div>
+          <div className="collapse-content text-green-500">
+            <p>
+              Engr. Chief Elias Mbam is a man that has established a robust
+              relationship among the leaders and across all the clans in the
+              State. From Ikwo Noyo, Ezza Ezekuna, Mgbolizhia, Igboesa, Ehugbo,
+              Ohanivo etc, he has built bridges of friendship that will
+              translate to bridges of more unity and understanding among the
+              clans and people of Ebonyi state. In Izzi clan, he&apos;s one man that
+              has evidently played down the kindred differences we are faced
+              with and has demonstrated in practical actions his readiness to
+              send such a devil induced difference to its early grave and
+              untimely death. For the records and to substantiate the position
+              above, Engr. Chief Mbam appointed an Igbojima his SA in his first
+              tenure, and in this his second tenure he appointed an Unwuera his
+              SA. Currently, he has given an Igbojima a sensitive appointment as
+              his Chief Press Secretary. In all these, he has demonstrated
+              clearly beyond any doubt that he&apos;ll treat all according to one&apos;s
+              merit and not based on kindred, when this happens, the differences
+              will surely die a natural death. Ebonyi needs a man of Engr Mbam&apos;s
+              experience, pedigree and integrity come 2023.
             </p>
           </div>
         </div>
