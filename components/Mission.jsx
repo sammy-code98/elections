6340fea15c5d73780caf2527d37f1a65fff8d82c
sammy-code98 <sammysmart98@yahoo.com>
2022-02-13 17:51:26 +0100

    feat:redesign mission section

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -11,56 +11,21 @@ export default function Mission() {
         STATEMENT
       </div>
 
-      <div className="grid  grid-cols-1rid md:grid-cols-2 md:gap-8 justify-items-center md:px-14 mt-12">
-        <div className="w-full bg-green-600 text-white rounded-md shadow-lg  mb-8 p-8">
-          <ul className="list-disc list-inside">
-            <div>
-              <li>A MAN OF DUE PROCESS:</li>
-              <p>
-                Due process simply means strict compliance/adherence to and
-                following established lay down rules/laws in getting things done
-                which running of a government is among. Tell me a man who
-                observes and respects rules and laws in his doings, and I&apos; ll
-                show you a man who will respect your rights and accord you all
-                your privileges. It&apos; s important to note that Engr. Chief Elias
-                Mbam is a product of quality trainings in both civil and public
-                service, and understands and appreciates the importance and the
-                fact that governance becomes easier when leadership is in strict
-                compliance to the rule of law, hence every public and civil
-                servant in the state is sure to enjoy all his rights and
-                privileges under his administration. As a product and a
-                beneficiary of well structured strong institutions, Chief Mbam
-                will restructure and reposition the civil service system for a
-                better and effective service delivery to Ebonyi people. With
-                Mbam, our rights shall be respected and privileges assuredly
-                guaranteed.
-              </p>
-            </div>
-          </ul>
-        </div>
-        <div className="w-full bg-green-600 text-white rounded-md shadow-lg mb-8 p-8">
-          <ul className="list-disc list-inside">
-            <li>
-              Coordinating communication between Engr. Elias Mbam Nwalem and the
-              masses.
-            </li>
-            <li>Provide support to state entities for the campaign process</li>
-            <li>
-              Mobilise Nigerians from different states to support the Engr.
-              Elias Mbam Nwalem Governorship Team
-            </li>
-            <li>
-              Prepare and administer an exploratory/outreach plan to Nigerians
-              that provides needed information to Engr. Elias Mbam Nwalem and
-              solicit meaningful information from them.
-            </li>
-            <li>
-              As a matter of course, take grassroots input into account, and
-              have as overarching goal the growth of the Engr. Elias Mbam Nwalem
-              Governorship Team
-            </li>
-          </ul>
-        </div>
+      <div className="flex justify-center justify-items-center mt-4 px-32 pt-4">
+
+<div className="collapse w-full bg-green-500 rounded-box shadow-md collapse-arrow">
+  <input type="checkbox" /> 
+  <div className="collapse-title text-xl text-white font-medium">
+    I open/close with click
+  </div> 
+  <div className="collapse-content text-white"> 
+    <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox instead of focus. Or you can force-open/force-close using 
+      <span className="badge badge-outline">collapse-open</span> and 
+      <span className="badge badge-outline">collapse-close</span> classes.
+    </p>
+  </div>
+</div> 
+
       </div>
     </section>
   );
