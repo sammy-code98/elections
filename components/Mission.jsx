446314fd9d8354d88f033be99e52ee916a012c31
sammy-code98 <sammysmart98@yahoo.com>
2022-02-13 18:03:30 +0100

    fix:update mission statement

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -11,21 +11,58 @@ export default function Mission() {
         STATEMENT
       </div>
 
-      <div className="flex justify-center justify-items-center mt-4 px-32 pt-4">
+      <div className="flex flex-col  mt-4 md:px-32 pt-4">
+        <div className="collapse w-full bg-green-500 rounded-box shadow-md mb-4 collapse-arrow">
+          <input type="checkbox" />
+          <div className="collapse-title text-base text-white font-medium">
+            A MAN OF DUE PROCESS{" "}
+          </div>
+          <div className="collapse-content text-white">
+            <p>
+              Due process simply means strict compliance/adherence to and
+              following established lay down rules/laws in getting things done
+              which running of a government is among. Tell me a man who observes
+              and respects rules and laws in his doings, and I&apos; ll show you
+              a man who will respect your rights and accord you all your
+              privileges. It&apos;s important to note that Engr. Chief Elias
+              Mbam is a product of quality trainings in both civil and public
+              service, and understands and appreciates the importance and the
+              fact that governance becomes easier when leadership is in strict
+              compliance to the rule of law, hence every public and civil
+              servant in the state is sure to enjoy all his rights and
+              privileges under his administration. As a product and a
+              beneficiary of well structured strong institutions, Chief Mbam
+              will restructure and reposition the civil service system for a
+              better and effective service delivery to Ebonyi people. With Mbam,
+              our rights shall be respected and privileges assuredly guaranteed.
+            </p>
+          </div>
+        </div>
 
-<div className="collapse w-full bg-green-500 rounded-box shadow-md collapse-arrow">
-  <input type="checkbox" /> 
-  <div className="collapse-title text-xl text-white font-medium">
-    I open/close with click
-  </div> 
-  <div className="collapse-content text-white"> 
-    <p>Collapse content reveals with focus. If you add a checkbox, you can control it using checkbox instead of focus. Or you can force-open/force-close using 
-      <span className="badge badge-outline">collapse-open</span> and 
-      <span className="badge badge-outline">collapse-close</span> classes.
-    </p>
-  </div>
-</div> 
-
+        <div className="collapse w-full bg-green-500 rounded-box shadow-md mb-4 collapse-arrow">
+          <input type="checkbox" />
+          <div className="collapse-title text-base text-white font-medium">
+            VERY WELL EXPERIENCED{" "}
+          </div>
+          <div className="collapse-content text-white">
+            <p>
+              With every sense of modesty, Engr. Elias Mbam is one of the most
+              experienced Ebonyian today most riped for governance and
+              leadership, looking at the wealth of experience he had raked up to
+              his credit from his years in civil service and as a public office
+              holder of different exalted positions with tasking
+              responsibilities. In all of the positions and his engagements in
+              civil and public services, he has always distinguished himself in
+              discharging every of his assigned duties. As a seasoned civil
+              servant, he&apos;s a trained beurocrat and as a long serving public
+              servant on the corridors of power, he&apos;s an excellent manager of
+              both human and material resources. Anyone that combines the above
+              two qualities will surely score A1 in practical leadership, and
+              Ebonyians are blessed to have one in the person of Engr Chief
+              Elias Mbam.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
