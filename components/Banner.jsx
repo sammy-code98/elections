864146437535fbed2202396fdfcd086f0922eaaf
sammy-code98 <sammysmart98@yahoo.com>
2022-03-18 01:14:10 +0100

    feat: add snowfall in banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,24 +1,31 @@
 /* eslint-disable @next/next/no-img-element */
+import Snowfall from "react-snowfall";
+
 export default function Banner() {
   return (
-    <div className="relative flex flex-row items-center w-full" style={{
-      maxHeight: "1000px",
-    }}>
+    <div
+      className="relative flex flex-row items-center w-full"
+      style={{
+        maxHeight: "1000px",
+      }}
+    >
+      <Snowfall snowflakeCount={200}/>
 
-      <img src="/men.jpg" alt="banner"  className="w-full h-full mt-20 xl:mt-0 md:mt-0" />
+      <img
+        src="/men.jpg"
+        alt="banner"
+        className="w-full h-full mt-20 xl:mt-0 md:mt-0"
+      />
 
       <div className="absolute top-0 left-0 h-full w-full bg-black opacity-40 "></div>
 
-      <div
-        className="mt-4 absolute top-0 h-full w-full"
-      >
+      <div className="mt-4 absolute top-0 h-full w-full">
         <div className="text-5xl sm:text-5xl md:text-7xl mt-24 text-left md:mt-72 xl:mt-72 p-3 text-white ">
           <div>Mbam</div>
           <div>Support</div>
           <div>Group</div>
         </div>
       </div>
-
     </div>
   );
 }
