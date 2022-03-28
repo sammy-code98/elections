e28fc605a9ec21e8219d444e1f0b9e389000845e
sammy-code98 <sammysmart98@yahoo.com>
2022-03-28 13:47:24 +0100

    update

--- components/Awards/Awards.jsx
+++ components/Awards/Awards.jsx
@@ -3,23 +3,21 @@ export default function Awards() {
   return (
     <div className="mt-24 md:32">
       <div className="text-3xl text-center text-gray-800">Recent Awards</div>
-      <div  className="grid  grid-cols-1 md:grid-cols-2 md:gap-6   md:px-96 mt-12 px-4">
+      {/* When other awards come in use md:grid-cols-3  and then reduce md:px to a lesser value */}
+      <div className="grid  grid-cols-1 md:grid-cols-2 md:gap-6   md:px-96 mt-12 px-4">
         <div className="m-2 ">
           <span className="text-base text-gray-800 font-bold">
-          Award For Prudent Financial Resource Management.
-
+            Award For Prudent Financial Resource Management.
           </span>
           <ProgressLine
-        
             visualParts={[
               {
-                percentage: "100%",
-                color: "blue",
+                percentage: "90%",
+                color: "green",
               },
             ]}
           />
         </div>
-      
       </div>
     </div>
   );
